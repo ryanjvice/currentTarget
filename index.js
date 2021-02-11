00bfe805f60887339f93da6b1462dc7065075aae
Ryan Vice <76831245+ryanjvice@users.noreply.github.com>
2021-02-11 17:50:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+$(function() {
+    $(".foo, p, ul, li").click(function(event) {
+        event.stopPropagation();
+        $("h2").text("event.currentTarget's text is: " + $
+        (event.currentTarget).text());
+    });
+});
